dc25a9680991e7a6e1de2d2ada9a3549e4e5f880
Tsewell <18008072409@163.com>
2021-06-09 15:10:15 +0800

    2.4 处理命令行参数

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(std::str::FromStr::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(),"Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d,*m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}",numbers,d)
 }
 
 fn gcd(mut n: u64,mut m: u64) -> u64 {
@@ -13,11 +32,4 @@ fn gcd(mut n: u64,mut m: u64) -> u64 {
         m = m % n;
     }
     n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14,15),1);
-
-    assert_eq!(gcd(36,8),4)
 }
